ad75268a15e65b5be3d4b997dd2aae598cc3baf4
Afonso Castro <up202208026@fe.up.pt>
2024-03-17 17:05:39 +0000

    Simple quick update of main menu

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -32,7 +32,6 @@ void Menu::run() {
             switch (decision[0]) {
                 case '1':
                     path = "../csv/Madeira/";
-
                     goto load;
                 case '2':
                     path = "../csv/Mainland Portugal/";
@@ -74,10 +73,15 @@ void Menu::run() {
         string decision;
         cout << string(LINE_SIZE_, '-') << endl;
         cout << "Select your operation:" << endl;
-        cout << "1 - Fuck off." << endl;
-        cout << "2 - Fuck off." << endl;
-        cout << "3 - Custom." << endl;
-        cout << "4 - Exit." << endl;
+        cout << string(LINE_SIZE_, '-') << endl;
+        cout << "1 - Maximum amount of reachable water of a given city." << endl; //T2.1
+        cout << "2 - List of maximum amount of reachable water per city." << endl; //T2.1
+        cout << "3 - Water reservoirs supply and demand status." << endl; //T2.2
+        cout << "4 - Network balancing tool." << endl; //T2.3
+        cout << "5 - Water reservoirs deactivation analysis." << endl; //T3.1
+        cout << "6 - Pumping stations deactivation analysis." << endl; //T3.2
+        cout << "7 - Critical pipes analysis." << endl; //T3.3
+        cout << "8 - Exit." << endl;
         cout << string(LINE_SIZE_, '-') << endl;
         cout << "Please select the task you wish to perform by inputting its number: ";
 
@@ -91,17 +95,24 @@ void Menu::run() {
                 case '2':
                     goto end;
                 case '3':
-                    cout << "Fuck Off \n";
                     goto end;
                 case '4':
+                    goto end;
+                case '5':
+                    goto end;
+                case '6':
+                    goto end;
+                case '7':
+                    goto end;
+                case '8':
                     cout << "Goodbye!" << endl;
                     exit(0);
                 default:
-                    cout << "Invalid input. Please enter a number between 1 and 4: ";
+                    cout << "Invalid input. Please enter a number between 1 and 8: ";
                     break;
             }
         }
         end:
-        cout << "Fucking Off \n";
+        cout << "Shutting Off \n";
     }
 }
